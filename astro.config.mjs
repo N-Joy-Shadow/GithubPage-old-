9c2abc6923aa70aac1337452a9d0e6f3109b108d
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2022-08-14 00:31:12 +0900

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact(), mdx(), tailwind()]
+});
