f2d690ef4137838c4a0c00bafde35b677f5e87ef
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2022-08-14 16:31:40 +0900

    side bar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	transform: {
+		mdx: (content) => {
+		  return remark().process(content)
+		}
+	},
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require('@tailwindcss/typography')],
+
 }
